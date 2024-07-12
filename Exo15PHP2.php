e0a579a271098e6190fd0d39258e38f3ee701c6e
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-12 16:56:03 +0200

    Exo15_Solu_PHP2

---
+++ Exo15PHP2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 15 PHP 2</title>
+</head>
+<body>
+
+    <h1>XV. Exercice 15 : Validation d'Adresse E-mail en PHP</h1>
+    <p>Vérifier si une adresse e-mail est au bon format en utilisant les ressources de la documentation PHP.</p>
+
+    <?php
+    // Adresse e-mail à vérifier
+    $email = "elan@elan-formation.fr";
+
+    // Utiliser filter_var pour valider l'adresse e-mail
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "L'adresse $email est une adresse e-mail valide";
+    } else {
+        echo "L'adresse $email n'est pas une adresse e-mail valide";
+    }
+    ?>
+
+</body>
+</html>
